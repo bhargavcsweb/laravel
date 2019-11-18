6a609dc1c11c3de518e2321c622a78e0d3ec64c2
bhargavcsweb <51404144+bhargavcsweb@users.noreply.github.com>
2019-11-18 17:52:09 +0530

    updated

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ Route::get('', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 
-
+Route::resource('post', 'PostsController');
